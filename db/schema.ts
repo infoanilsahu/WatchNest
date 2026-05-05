d899f0237422b33b0b598a5ed20721d96c68874c
infoanilsahu <info.anilsahu@gmail.com>
2026-05-05 10:06:34 +0530

    database table schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, varchar,text } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, text, timestamp, pgEnum } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,6 +6,39 @@ export const usersTable = pgTable("users", {
   accessToken: text("access_token"),
   refreshToken: text("refresh_token"),
   tokenExpiresAt: integer("token_expires_at"),
+});
+
+
+export const accountTable = pgTable("account", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  username: varchar().unique().notNull(),
+  name: varchar().notNull(),
+
+  userId: integer().notNull().references(() => usersTable.id)
+});
+
+export const videosTable = pgTable("videos", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar().notNull(),
+  description: varchar(),
+  link: varchar().notNull(),
+  timestamp: timestamp().defaultNow().notNull(),
+
+  playlistId: integer().references(() => playlistsTable.id),
+  accountId: integer().notNull().references(() => accountTable.id),
+});
+
+const visibleEnum = pgEnum("visible", ["public", "private"]);
+
+
+export const playlistsTable = pgTable("playlists", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar().notNull(),
+  description: varchar(),
+  timestamp: timestamp().defaultNow().notNull(),
+  visble: visibleEnum().notNull().default("private"),
+
+  accountId: integer().notNull().references(() => accountTable.id)
 });
 
 
