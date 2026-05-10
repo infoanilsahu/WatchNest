07c36ea8c8c3f3c76c3143772afe4df8c6a485b6
infoanilsahu <info.anilsahu@gmail.com>
2026-05-10 18:23:02 +0530

    user playlist data route

--- app/api/playlist/route.ts
+++ app/api/playlist/route.ts
@@ -1,18 +1,65 @@
+import { db } from "@/db/db";
+import { playlistsTable, videosTable } from "@/db/schema";
 import { TokenData } from "@/lib/tokenData";
+import { userPaylistData } from "@/validation/userData";
+import { and, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     try {
 
+        let tokenData;
+
         try {
 
-            const {accountId, email, userId} = await TokenData("myJwt")
+            tokenData = await TokenData("myJwt")
             
         } catch (err: unknown) {
             return NextResponse.json({
                 message: err instanceof Error ? err.message : "Server Error"
             }, { status: 401 });
         }
+
+        const {accountId, email, userId} = tokenData
+
+        const json = await req.json()
+        const parseJson = userPaylistData.safeParse(json)
+        if( !parseJson.success ) {
+            return NextResponse.json({
+                message: parseJson.error.message
+            }, {status: 400})
+        }
+
+        const { userId: reqUserId, playlistId } = parseJson.data
+        if( reqUserId !== userId ) {
+            return NextResponse.json({
+                message: "invalid token"
+            }, {status: 400})
+        }
+
+        const playlist = await db.select().from(playlistsTable).where(
+            and(
+                eq(playlistsTable.id, playlistId),
+                eq(playlistsTable.accountId, accountId)
+            )
+        ).then((res) => res[0])
+        if( !playlist ) {
+            return NextResponse.json({
+                message: "playlist not found"
+            }, {status: 400})
+        }
+
+        const videos = await db.select().from(videosTable).where(
+            and(
+                eq(videosTable.playlistId, playlistId),
+                eq(videosTable.accountId, accountId)
+            )
+        )
+
+        return NextResponse.json({
+            playlistName: playlist,
+            videos: videos
+        }, {status: 200})
 
         
         
