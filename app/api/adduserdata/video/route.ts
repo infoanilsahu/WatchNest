fb8fbe435feb87fa00db7d0fb9958c4dc5723e90
infoanilsahu <info.anilsahu@gmail.com>
2026-05-11 20:56:54 +0530

    add video in database

---
+++ app/api/adduserdata/video/route.ts
@@ -0,0 +1,61 @@
+import { db } from "@/db/db";
+import { videosTable } from "@/db/schema";
+import { TokenData } from "@/lib/tokenData";
+import { addVideo } from "@/validation/addUserData";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+
+        let tokenData;
+        
+        try {
+
+            tokenData = await TokenData("myJwt")
+
+        } catch (err: unknown) {
+            return NextResponse.json({
+                message: err instanceof Error ? err.message : "Server Error"
+            }, { status: 401 });
+        }
+    
+        const { accountId, email, userId } = tokenData
+
+        const json = await req.json()
+        const parseJson = addVideo.safeParse(json)
+        if( !parseJson.success ) {
+            return NextResponse.json({
+                message: parseJson.error.message
+            }, { status: 400 })
+        }
+
+        const {title, description, link} = parseJson.data
+
+        const video = await db.insert(videosTable).values({
+            title,
+            description,
+            link,
+            accountId
+        }).returning().then((res) => res[0])
+
+        if( !video ) {
+            return NextResponse.json({
+                message: "video not uploaded"
+            }, {status: 400})
+        }
+
+        return NextResponse.json({
+            message: "video uploaded successfully",
+        }, {status: 200})
+        
+
+        
+    } catch (err: unknown) {
+        console.error("server error: ", err)
+
+        return NextResponse.json({
+            message: "server error"
+        }, { status: 500 })
+
+    }
+}
---
+++ validation/addUserData.ts
@@ -0,0 +1,7 @@
+import { number, object, string, ZodISODateTime } from "zod";
+
+export const addVideo = object({
+    title: string(),
+    description: string(),
+    link: string().startsWith("https:"),
+})
