79259690e3a8beea3a10606b5e57329407d47f53
infoanilsahu <info.anilsahu@gmail.com>
2026-05-14 19:15:58 +0530

    upload video inside playlist

--- validation/addUserData.ts
+++ validation/addUserData.ts
@@ -2,7 +2,7 @@ import { number, object, string, enum as Enum } from "zod";
 
 export const addVideo = object({
     title: string(),
-    description: string(),
+    description: string().optional(),
     link: string().url({protocol: /^https$/}),
 })
 
@@ -12,5 +12,12 @@ export const addPlaylist = object({
     title: string(),
     description: string().optional(),
     visible: visibleEnum,
+})
+
+export const addPlaylistVideo = object({
+    title: string(),
+    description: string().optional(),
+    link: string().url({protocol: /^https$/}),
+    playlistId: number(),
 })
 
