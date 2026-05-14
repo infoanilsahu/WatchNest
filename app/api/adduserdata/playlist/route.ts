80860ef2af8dc141b240553f8d19221644e8bb4c
infoanilsahu <info.anilsahu@gmail.com>
2026-05-14 18:47:15 +0530

    sever data may leak

--- app/api/adduserdata/playlist/route.ts
+++ app/api/adduserdata/playlist/route.ts
@@ -14,8 +14,9 @@ export async function POST(req: NextRequest) {
             tokenData = await TokenData("myJwt")
 
         } catch (err: unknown) {
+            console.log("auth err: ", err)
             return NextResponse.json({
-                message: err instanceof Error ? err.message : "Server Error"
+                message: "Unauthorized"
             }, { status: 401 });
         }
     
